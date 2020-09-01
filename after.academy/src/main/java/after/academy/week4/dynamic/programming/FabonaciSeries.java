1b772c9c88a3b408951f43ff78a9efdad0ec8fb0
writetogurpreetsingh8 <writetogurpreetsingh8@gmail.com>
2020-09-01 19:53:47 +0530

    adding devFile.java into development repo

--- after.academy/src/main/java/after/academy/week4/dynamic/programming/FabonaciSeries.java
+++ after.academy/src/main/java/after/academy/week4/dynamic/programming/FabonaciSeries.java
@@ -1,11 +1,12 @@
 package after.academy.week4.dynamic.programming;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.Date;
 
 public class FabonaciSeries {
 	
-private static long fabo(int n,long[] array) {
-		
+	private static long fabo(int n,long[] array) {
 		if(n <=1 ) {
 			return n;
 		}
@@ -15,10 +16,26 @@ private static long fabo(int n,long[] array) {
 		array[n] = fabo(n-1,array)+fabo(n-2,array);
 		return array[n];
 	}
+
+	private static int facto(int n) {
+	
+	if( n == 1) {
+		return n;
+	}
+	return n*facto(n-1);
+}
 	public static void main(String[] args) {
+		
 		System.out.println(new Date());
-		int n = 5;
+		int n = 25;
 		System.out.println(FabonaciSeries.fabo(n,new long[n+1]));
 		System.out.println(new Date());
+		System.out.println(FabonaciSeries.facto(5));
+		
+		Integer p [][] = (Integer[][]) Array.newInstance(Integer.class,2,3);
+		System.out.println(p);
+		Arrays.fill(p[0], -1);
+		System.out.println(p);
+		
 	}
 }
